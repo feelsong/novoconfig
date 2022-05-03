9091a2c7b3557032c52f2c5848851b265a04517b
bitsko <26992310+bitsko@users.noreply.github.com>
2022-05-02 23:38:44 -0700

    Create ras64novo.sh

---
+++ ras64novo.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#there is no source code for this script to work with
+#untested
+#consult ur lawyer
+
+#need to fill these in
+username=
+rpcpassword=
+
+echo "raspi 64 bit debian novobitcoin compile script"
+
+#sourceDir=novobitcoin
+#source=https://github.com/wszhang/"$sourceDir"
+#git clone "$source"
+#cd "$sourceDir"
+
+sudo apt-get install \
+	build-essential libtool autotools-dev automake pkg-config \
+	bsdmainutils python3 libevent-dev libboost-system-dev libboost-filesystem-dev \
+	libboost-chrono-dev libboost-program-options-dev libboost-test-dev \
+	libboost-thread-dev libsqlite3-dev libqrencode-dev g++-arm-linux-gnueabihf \
+	curl libdb-dev libdb++-dev
+
+./autogen.sh
+
+CONFIG_SITE=$PWD/depends/arm-linux-gnueabihf/share/config.site \
+	./configure --without-gui --enable-reduce-exports LDFLAGS=-static-libstdc++
+
+make -j 3
+
+echo "nwal(){ \$HOME/.novo-bitcoin/bin/novobitcoin-cli getwalletinfo; }" >> ~/.bash_aliases
+echo "ninfo(){ \$HOME/.novo-bitcoin/bin/novobitcoin-cli getinfo; }" >> ~/.bash_aliases
+echo "nhelp(){ \$HOME/.novo-bitcoin/bin/novobitcoin-cli help; }" >> ~/.bash_aliases
+echo "nstart(){ \$HOME/.novo-bitcoin/bin/nbsv.sh; }" >> ~/.bash_aliases
+echo "ncli(){ \$HOME/.novo-bitcoin/bin/novobitcoin-cli \$1 \$2 \$3 \$4 \$5 \$6 \$7 \$8 \$9; }" >> ~/.bash_aliases
+#echo "update() { sudo apt update && sudo apt upgrade -y; }" >> ~/.bash_aliases
+echo "source ~/.bash_aliases or restart to use aliases such as nstart nwal and ncli"
+
+if [[ ! -d "$HOME/.novo-bitcoin" ]]; then
+	mkdir "$HOME/.novo-bitcoin"
+fi
+
+novoConf="$HOME/.novo-bitcoin/novo.conf"
+echo "port=8666"$'\n'"rpcport=8665"$'\n'"rpcuser=$username"$'\n'\
+	"rpcpassword=$rpcpassword"$'\n'"gen=1" > "$novoConf"
