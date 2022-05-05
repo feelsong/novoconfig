6e913dcf8209ec5613a3f8784297415921d8178b
bitsko <26992310+bitsko@users.noreply.github.com>
2022-05-04 19:54:58 -0700

    Update manjaro64raspi.sh

--- manjaro64raspi.sh
+++ manjaro64raspi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+sudo pacman -Syu
 sudo pacman -S libevent boost-libs sqlite qrencode \
 	binutils arm-none-eabi-binutils arm-none-eabi-gcc boost \
 	autoconf automake binutils bison fakeroot file \
 	findutils flex gawk gcc gettext grep groff gzip libtool \
-	m4 make patch pkgconf sed texinfo which miniupnpc
+	m4 make patch pkgconf sed texinfo which miniupnpc screen
 ./autogen.sh
 CONFIG_SITE=$PWD/depends/arm-linux-gnueabihf/share/config.site ./configure --without-gui --enable-reduce-exports LDFLAGS=-static-libstdc++
-make 
+make -j 3
